43d9abb158b294a0fc84009f2d2d3945ce2127bf
Yisong Chang <changyisong@ict.ac.cn>
2022-05-08 18:55:08 +0800

    riscv: serve: add support to ZynqMP prototypings

--- arch/riscv/include/asm/sbi.h
+++ arch/riscv/include/asm/sbi.h
@@ -8,37 +8,45 @@
 
 #include <linux/types.h>
 
-#define SBI_SET_TIMER 0
-#define SBI_CONSOLE_PUTCHAR 1
-#define SBI_CONSOLE_GETCHAR 2
-#define SBI_CLEAR_IPI 3
-#define SBI_SEND_IPI 4
-#define SBI_REMOTE_FENCE_I 5
-#define SBI_REMOTE_SFENCE_VMA 6
+#define SBI_SET_TIMER              0
+#define SBI_CONSOLE_PUTCHAR        1
+#define SBI_CONSOLE_GETCHAR        2
+#define SBI_CLEAR_IPI              3
+#define SBI_SEND_IPI               4
+#define SBI_REMOTE_FENCE_I         5
+#define SBI_REMOTE_SFENCE_VMA      6
 #define SBI_REMOTE_SFENCE_VMA_ASID 7
-#define SBI_SHUTDOWN 8
+#define SBI_SHUTDOWN               8
 
-#define SBI_CALL(which, arg0, arg1, arg2, arg3) ({		\
+struct sbi_call_res {
+	unsigned long a0;
+	unsigned long a1;
+};
+
+#define SBI_CALL_RET(extid, funcid, outval, arg0, arg1, arg2, arg3) ({    \
 	register uintptr_t a0 asm ("a0") = (uintptr_t)(arg0);	\
 	register uintptr_t a1 asm ("a1") = (uintptr_t)(arg1);	\
 	register uintptr_t a2 asm ("a2") = (uintptr_t)(arg2);	\
 	register uintptr_t a3 asm ("a3") = (uintptr_t)(arg3);	\
-	register uintptr_t a7 asm ("a7") = (uintptr_t)(which);	\
+	register uintptr_t a6 asm ("a6") = (uintptr_t)(funcid);	\
+	register uintptr_t a7 asm ("a7") = (uintptr_t)(extid);	\
 	asm volatile ("ecall"					\
-		      : "+r" (a0)				\
-		      : "r" (a1), "r" (a2), "r" (a3), "r" (a7)	\
+		      : "+r" (a0), "+r" (a1)			\
+		      : "r" (a2), "r" (a3), "r"(a6), "r" (a7)	\
 		      : "memory");				\
+	*(outval) = a1; \
 	a0;							\
 })
 
-/* Lazy implementations until SBI is finalized */
-#define SBI_CALL_0(which) SBI_CALL(which, 0, 0, 0, 0)
-#define SBI_CALL_1(which, arg0) SBI_CALL(which, arg0, 0, 0, 0)
-#define SBI_CALL_2(which, arg0, arg1) SBI_CALL(which, arg0, arg1, 0, 0)
-#define SBI_CALL_3(which, arg0, arg1, arg2) \
-		SBI_CALL(which, arg0, arg1, arg2, 0)
-#define SBI_CALL_4(which, arg0, arg1, arg2, arg3) \
-		SBI_CALL(which, arg0, arg1, arg2, arg3)
+#define SBI_CALL(which, funcid, arg0, arg1, arg2, arg3)	\
+		struct sbi_call_res res;  \
+		res.a0 = SBI_CALL_RET((which), (funcid), &res.a1, (arg0), (arg1), (arg2), (arg3));
+
+#define SBI_CALL_0(which) SBI_CALL(which, 0, 0, 0, 0, 0)
+#define SBI_CALL_1(which, arg0) SBI_CALL(which, 0, arg0, 0, 0, 0)
+#define SBI_CALL_2(which, arg0, arg1) SBI_CALL(which, 0, arg0, arg1, 0, 0)
+#define SBI_CALL_3(which, arg0, arg1, arg2) SBI_CALL(which, 0, arg0, arg1, arg2, 0)
+#define SBI_CALL_4(which, arg0, arg1, arg2, arg3) SBI_CALL(which, 0, arg0, arg1, arg2, arg3)
 
 static inline void sbi_console_putchar(int ch)
 {
@@ -47,7 +55,8 @@ static inline void sbi_console_putchar(int ch)
 
 static inline int sbi_console_getchar(void)
 {
-	return SBI_CALL_0(SBI_CONSOLE_GETCHAR);
+	SBI_CALL_0(SBI_CONSOLE_GETCHAR);
+	return res.a0;
 }
 
 static inline void sbi_set_timer(uint64_t stime_value)
